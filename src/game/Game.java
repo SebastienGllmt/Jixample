e66252916722a35aa9a6be0439747f50a3f9956a
Sebastien <SebastienGllmt@gmail.com>
2013-02-03 16:37:53 -0800

    Implementing new map system

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,6 @@
 package game;
 
+import gui.JixelSprite;
 import stage.JixelGame;
 
 public class Game extends JixelGame {
@@ -13,13 +14,14 @@ public class Game extends JixelGame {
 
 	public Game() {
 		super("Game", width, height, 1, tileSize, 60, 20);
-
 		player = new Player("res/you.png", "Player", width / 2, height / 2, 1, 2, 10);
 		test = new Enemy("res/evil.png", "Evil", width / 2 + 5 * tileSize, height / 2, 1, 1, 5);
 		getEntityList().add(player);
 		getEntityList().add(test);
 		getScreen().lockOn(player);
 		getVM().newVar("name", "Sebastien");
+		getMap().setSpriteSheet(new JixelSprite("res/tiles.png"));
+		getMap().loadLevel("res/map.jixmap");
 	}
 
 	boolean lockOnPlayer = true;
