8ba4b08dc8ed55eeadc6bb5e4c1f0f774d4744cd
Sebastien <SebastienGllmt@gmail.com>
2013-02-22 16:15:17 -0800

    Added collision detection

--- src/game/Enemy.java
+++ src/game/Enemy.java
@@ -1,6 +1,9 @@
 package game;
 
-public class Enemy extends JixampleEntity{
+import jixel.entity.JixelCollidable;
+import jixel.entity.JixelEntity;
+
+public class Enemy extends JixampleEntity implements JixelCollidable{
 
 	public Enemy(final String imgPath, String animPath, String name, int width, int height, int x, int y, double friction, double acceleration, double maxSpeed) {
 		super(imgPath, animPath, name, width, height, x, y, friction, acceleration, maxSpeed);
@@ -8,6 +11,24 @@ public class Enemy extends JixampleEntity{
 
 	@Override
 	public void update() {
+		
+	}
+
+	@Override
+	public void isColliding(JixelEntity e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onHit(JixelEntity e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSeparation(JixelEntity e) {
+		// TODO Auto-generated method stub
 		
 	}
 
--- src/game/Player.java
+++ src/game/Player.java
@@ -1,8 +1,10 @@
 package game;
 
+import jixel.entity.JixelCollidable;
+import jixel.entity.JixelEntity;
 import jixel.stage.JixelGame;
 
-public class Player extends JixampleEntity {
+public class Player extends JixampleEntity implements JixelCollidable {
 
 	public Player(final String imgPath, String animPath, String name, int width, int height, int x, int y, double friction, double acceleration, double maxSpeed) {
 		super(imgPath, animPath, name, width, height, x, y, friction, acceleration, maxSpeed);
@@ -33,6 +35,21 @@ public class Player extends JixampleEntity {
 		}
 		
 		super.update();
+	}
+
+	@Override
+	public void isColliding(JixelEntity e) {
+		
+	}
+
+	@Override
+	public void onHit(JixelEntity e) {
+		System.out.println("On hit " + e.getName());
+	}
+
+	@Override
+	public void onSeparation(JixelEntity e) {
+		System.out.println("On separation " + e.getName());
 	}
 
 }
